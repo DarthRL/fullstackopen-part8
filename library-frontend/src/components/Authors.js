a94bf40711ca1326b78a2ed9cf515a55a30a136a
DarthRL <darthrlu@gmail.com>
2023-06-30 16:42:04 +0800

    8.8

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,8 +1,27 @@
+import { gql, useQuery } from "@apollo/client"
+
 const Authors = (props) => {
+  const ALL_AUTHORS = gql`
+    query {
+      allAuthors {
+        name
+        born
+        bookCount
+      }
+    }
+  `
+  const result = useQuery(ALL_AUTHORS, {
+      fetchPolicy: "no-cache" 
+  })
+
   if (!props.show) {
     return null
   }
-  const authors = []
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+  const authors = result.data.allAuthors
 
   return (
     <div>
