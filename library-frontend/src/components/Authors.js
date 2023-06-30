57792e1f8b6038ffd8dbaa863edd580e1a758d22
DarthRL <darthrlu@gmail.com>
2023-06-30 17:50:57 +0800

    8.11

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,8 +1,14 @@
-import { useQuery } from "@apollo/client"
-import { ALL_AUTHORS } from "../queries"
+import { useMutation, useQuery } from '@apollo/client'
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
+import { useState } from 'react'
 
-const Authors = (props) => {
+const Authors = props => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
   const result = useQuery(ALL_AUTHORS)
+  const [changeBorn] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  })
 
   if (!props.show) {
     return null
@@ -12,6 +18,15 @@ const Authors = (props) => {
     return <div>loading...</div>
   }
   const authors = result.data.allAuthors
+
+  const submit = event => {
+    event.preventDefault()
+
+    changeBorn({ variables: { name, setBornTo: parseInt(born) } })
+
+    setName('')
+    setBorn('')
+  }
 
   return (
     <div>
@@ -23,7 +38,7 @@ const Authors = (props) => {
             <th>born</th>
             <th>books</th>
           </tr>
-          {authors.map((a) => (
+          {authors.map(a => (
             <tr key={a.name}>
               <td>{a.name}</td>
               <td>{a.born}</td>
@@ -32,6 +47,25 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <h3>Set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          ></input>
+        </div>
+        <div>
+          born
+          <input
+            type='number'
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          ></input>
+        </div>
+        <button type='submit'>update author</button>
+      </form>
     </div>
   )
 }
